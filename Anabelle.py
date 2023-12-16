89cdfd349bc89d6aecc1fdddbb07beab8e5203ea
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2023-12-16 17:30:28 +0530

    Update Anabelle.py

--- Anabelle.py
+++ Anabelle.py
@@ -1,19 +1,20 @@
-from pyrogram import User, __version__
-from config import *
-import logging
-from os import path, mkdir
-from sys import stdout
 from datetime import datetime
 from logging import INFO, WARNING, FileHandler, StreamHandler, basicConfig, getLogger
+from os import path, mkdir
+from sys import stdout
+from pyrogram import User, __version__
+from config import *
 
+# Constants Naming Convention
 LOG_DATETIME = datetime.now().strftime("%d_%m_%Y-%H_%M_%S")
-LOGDIR = f"logs"  
+LOGDIR = "logs"  
 
 if not path.isdir(LOGDIR):
     mkdir(LOGDIR)
 
 LOGFILE = f"{LOGDIR}/{__name__}_{LOG_DATETIME}_log.txt"
 
+# Imports Order
 file_handler = FileHandler(filename=LOGFILE)
 stdout_handler = StreamHandler(stdout)
 
@@ -27,19 +28,16 @@ getLogger("pyrogram").setLevel(WARNING)
 logger = getLogger(__name__)
 
 art = """
-
    _____                      ___.          .__  .__          
   /  _  \   ____   ____ _____ \_ |__   ____ |  | |  |   ____  
- /  /_\  \ /    \ /    \\__  \ | __ \_/ __ \|  | |  | _/ __ \ 
+ /  /_\  \ /    \ /    \ \__  \ | __ \_/ __ \|  | |  | _/ __ \ 
 /    |    \   |  \   |  \/ __ \| \_\ \  ___/|  |_|  |_\  ___/ 
 \____|__  /___|  /___|  (____  /___  /\___  >____/____/\___  >
         \/     \/     \/     \/    \/     \/               \/ 
-
-
 """
 
 Annabelle = User(name="Annabelle", api_id=API_ID, api_hash=API_HASH,
-              session_string=SESSION_STRING, plugins={"root": "annabelle/modules"})
+                  session_string=SESSION_STRING, plugins={"root": "annabelle/modules"})
 
 logger.info(art)
 
