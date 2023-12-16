1c7efcfa8a1d4fa7e075d19c8b7bc19a35854168
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2023-12-16 09:52:52 +0530

    Update alive.py

--- annabelle/modules/alive.py
+++ annabelle/modules/alive.py
@@ -1,6 +1,5 @@
 from config import MY_ID
 from annabelle.helper_funcs.strings import ALIVE_TXT
-from config ALIVE_EMOJI
 from annabelle import Annabelle 
 from pyrogram import filters as vrn
 
@@ -9,5 +8,5 @@ DEFAULT_IMG = "https://telegra.ph/file/0cf9d9cea0eebd03f6c1e.jpg"
 async def alive(Annabelle, message):
      if message.from_user.id == MY_ID:
          await message.reply_photo(photo=DEFAULT_IMG,
-                                   caption=ALIVE_TXT.format(ALIVE_EMOJI)
+                                   caption=ALIVE_TXT.
               )
