791dd6a92f1624ab6abd193e57e33cc6e2daa856
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2023-12-16 17:23:37 +0530

    Update autobio.py

--- annabelle/modules/autobio.py
+++ annabelle/modules/autobio.py
@@ -7,19 +7,19 @@ from config import MY_ID, HANDLER
 from pyrogram.types.messages_and_media import Message
 
 @Annabelle.on_message(vrn.command("autobio", HANDLER))
-async def autobio(bot:Annabelle, msg:Message) :
+async def autobio(bot: Annabelle, msg: Message):
     value = msg.text.split(None, 1)
     args = value[1].lower()
-    if args == "true" :
+    if args == "true":
         await msg.edit_text(text="**__Plugin Enabled__** : `autobio`")
-        while value is "true" :
+        while value == "true":
             await asyncio.sleep(60)
             hour = datetime.datetime.now(pytz.timezone("Asia/Kolkata")).hour
             minute = datetime.datetime.now(pytz.timezone("Asia/Kolkata")).minute
             await bot.update_profile(bio=f"H : {hour} , M : {minute}")
 
-    elif args == "false" :
+    elif args == "false":
         await msg.edit_text(text="**__Plugin Disabled__** : `autobio`")
 
-    else :
+    else:
         await msg.edit_text(text="**__Please input a boolean as a parameter !!__**")
