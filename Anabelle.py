80fb5a119c1c0acd9ba5c0761955fa1df427adea
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2023-12-16 09:17:20 +0530

    Update Anabelle.py

--- Anabelle.py
+++ Anabelle.py
@@ -1,6 +1,30 @@
 from pyrogram import Client, __version__
 from config import *
 import logging
+from os import path, mkdir
+from sys import stdout
+from datetime import datetime
+from logging import INFO, WARNING, FileHandler, StreamHandler, basicConfig, getLogger
+
+LOG_DATETIME = datetime.now().strftime("%d_%m_%Y-%H_%M_%S")
+LOGDIR = f"{__name__}/logs"
+
+if not path.isdir(LOGDIR):
+    mkdir(LOGDIR)
+
+LOGFILE = f"{LOGDIR}/{__name__}_{LOG_DATETIME}_log.txt"
+
+file_handler = FileHandler(filename=LOGFILE)
+stdout_handler = StreamHandler(stdout)
+
+basicConfig(
+    format="%(asctime)s - [Annabelle] - %(levelname)s - %(message)s",
+    level=INFO,
+    handlers=[file_handler, stdout_handler],
+)
+
+getLogger("pyrogram").setLevel(WARNING)
+logger = getLogger(__name__)
 
 art = """
 
