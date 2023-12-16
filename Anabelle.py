9fcbf7b16df0015c948f14a065f4b80b82613ef4
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2023-12-16 09:21:30 +0530

    Update Anabelle.py

--- Anabelle.py
+++ Anabelle.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from logging import INFO, WARNING, FileHandler, StreamHandler, basicConfig, getLogger
 
 LOG_DATETIME = datetime.now().strftime("%d_%m_%Y-%H_%M_%S")
-LOGDIR = f"{__name__}/logs"
+LOGDIR = f"logs"  
 
 if not path.isdir(LOGDIR):
     mkdir(LOGDIR)
