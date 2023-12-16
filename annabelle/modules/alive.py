3d28ec4ac70e6808c99f36a37673e4ffbdfc96af
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2023-12-16 09:55:10 +0530

    Update alive.py

--- annabelle/modules/alive.py
+++ annabelle/modules/alive.py
@@ -4,9 +4,11 @@ from annabelle import Annabelle
 from pyrogram import filters as vrn
 
 DEFAULT_IMG = "https://telegra.ph/file/0cf9d9cea0eebd03f6c1e.jpg"
+
 @Annabelle.on_message(vrn.command('alive', HANDLER))
 async def alive(Annabelle, message):
-     if message.from_user.id == MY_ID:
-         await message.reply_photo(photo=DEFAULT_IMG,
-                                   caption=ALIVE_TXT.
-              )
+    if message.from_user.id == MY_ID:
+        await message.reply_photo(
+            photo=DEFAULT_IMG,
+            caption=ALIVE_TXT
+        )
