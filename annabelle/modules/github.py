7ed0b38be022cd0168a5e3d5fd1dc41ab2beb0df
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2023-12-16 13:27:01 +0530

    Update github.py

--- annabelle/modules/github.py
+++ annabelle/modules/github.py
@@ -1,43 +1,42 @@
 import json
 import requests
-from Anabelle import Annabelle
+from Anabelle import Annabelle 
 from config import MY_ID, HANDLER
 from pyrogram import filters as vrn
 from pyrogram.types.messages_and_media import Message
 
-
-@Annabelle.on_message(vrn.command("github", HANDLER))
+@Annabelle.on_message(filters.command("github", HANDLER))
 async def github(bot:Annabelle, msg:Message) :
     if msg.from_user.id == MY_ID :
         args = msg.text.split(None, 1)
         if len(args) >= 2:
             query = args[1]
             await msg.edit("`searching github...`")
-            try :
-              get_url = f"https://api.github.com/search/users?q={query}"
-              request1 = requests.get(get_url)
-              json_data = request1.json()
-              raw_data = json_data["items"]
-              data_ = raw_data[0]
-              url = data_["html_url"]
-              request2 = requests.get(url)
-              data = request2.json()
-              await msg.edit_text(
-    text=f"""
-<b>__Stdout__</b>
-<b>Query</b>: <code>{query}</code>
+            try:
+                get_url = f"https://api.github.com/search/users?q={query}"
+                request1 = requests.get(get_url)
+                json_data = request1.json()
+                raw_data = json_data["items"]
+                data_ = raw_data[0]
+                url = data_["html_url"]
+                request2 = requests.get(url)
+                data = request2.json()
+                await msg.edit_text(text=f"""<p> **__Stdout__**
+**Query** : `{query}`
 
-<b>ID</b>: <code>{data["id"]}</code>
-<b>Url</b>: <a href="{url}">ʟɪɴᴋ</a>
-<b>Type</b>: <code>{data["type"]}</code>
-<b>Name</b>: <code>{data["name"]}</code>
-<b>Login</b>: <code>{data["login"]}</code>
-<b>Public Repos</b>: <code>{data["public_repos"]}</code>
-<b>Following</b>: <code>{data["following"]}</code>
-<b>Followers</b>: <code>{data["followers"]}</code>
-<b>Email</b>: <code>{data["email"]}</code>
+**ID** : `{data["id"]}`
+**Url** : <a href="{url}"> ʟɪɴᴋ </a>
+**Type** : `{data["type"]}`
+**Name** : `{data["name"]}`
+**Login** : `{data["login"]}`
+**Public Repos** : `{data["public_repos"]}`
+**Following** : `{data["following"]}`
+**Followers** : `{data["followers"]}`
+**Email** : `{data["email"]}`
 
-<b>ANABELLE USERBOT</b>
-""",
-    parse_mode="html"
-)
+**ANABELLE USERBOT**
+""")
+            except:
+                await msg.edit_text(text=f"**Couldnt find results for** `{query}`")
+        else:
+            await msg.edit("`Give a username to search`")
