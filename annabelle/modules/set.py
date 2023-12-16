acfc38a22827bdfc0f4ccf6f48adedf0e7da4980
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2023-12-16 17:05:33 +0530

    Update set.py

--- annabelle/modules/set.py
+++ annabelle/modules/set.py
@@ -3,25 +3,24 @@ from pyrogram import filters as vrn
 
 @Annabelle.on_message(vrn.command('setemoji', HANDLER))
 async def setemoji(Annabelle, message):
-      if message.from_user.id == MY_ID:
-          args = message.text.split(None, 1)
-          if len(args) >= 2:
-             ALIVE_EMOJI = args[1]
-             await message.edit(f"`ALIVE_EMOJI has been set to {ALIVE_EMOJI}")
-            
-          else:
-             await message.edit("`At least mention what to reply with!`")
+    if message.from_user.id == MY_ID:
+        args = message.text.split(None, 1)
+        if len(args) >= 2:
+            ALIVE_EMOJI = args[1]
+            await message.edit(f"`ALIVE_EMOJI has been set to {ALIVE_EMOJI}")
+        else:
+            await message.edit("`At least mention what to reply with!`")
 
 @Annabelle.on_message(vrn.command('alivepic', HANDLER))
 async def setpic(Annabelle, message):
-      if message.from_user.id == MY_ID:
-         args = message.text.split(None, 1)
-         if len(args) >= 2:
+    if message.from_user.id == MY_ID:
+        args = message.text.split(None, 1)
+        if len(args) >= 2:
             x = args[1]
-            if x.endswith["jpg", "jpeg", "png"]:
-               ALIVE_IMG = x
-               await message.edit(f"ALIVE_IMG has been set to {ALIVE_IMG}")
+            if x.endswith(("jpg", "jpeg", "png")):
+                ALIVE_IMG = x
+                await message.edit(f"ALIVE_IMG has been set to {ALIVE_IMG}")
             else:
-               await message.edit("`Available types are jpg, jpeg, png.`")
+                await message.edit("`Available types are jpg, jpeg, png.`")
         else:
-           await message.edit("`Set to what?`")
+            await message.edit("`Set to what?`")
