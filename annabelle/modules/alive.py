cc45bdb33245dcf083c92ebb3d0724268fdaa341
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2023-12-16 17:47:41 +0530

    Update alive.py

--- annabelle/modules/alive.py
+++ annabelle/modules/alive.py
@@ -1,3 +1,4 @@
+import logging
 from config import MY_ID, HANDLER
 from annabelle.helper_funcs.strings import ALIVE_TXT
 from Anabelle import Annabelle
@@ -5,10 +6,29 @@ from pyrogram import filters as vrn
 
 DEFAULT_IMG = "https://telegra.ph/file/0cf9d9cea0eebd03f6c1e.jpg"
 
+# Configure the logger
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
 @Annabelle.on_message(vrn.command('alive', HANDLER))
-async def alive(Annabelle, message):
+async def alive_command_handler(client, message):
+    """
+    Command handler for the 'alive' command.
+    Replies with a photo and caption.
+    """
     if message.from_user.id == MY_ID:
-        await message.reply_photo(
-            photo=DEFAULT_IMG,
-            caption=ALIVE_TXT
-        )
+        try:
+            await message.reply_photo(
+                photo=DEFAULT_IMG,
+                caption=ALIVE_TXT
+            )
+            logger.info("Alive message sent successfully.")
+        except Exception as e:
+            # Log the exception
+            logger.error(f"Error sending alive message: {str(e)}")
+            
+            # Handle exceptions if photo sending fails
+            await message.reply_text(f"Error sending alive message: {str(e)}")
