2b9846b1611d19b809bc7acaab8506c67a469f98
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2023-12-16 17:52:29 +0530

    Update help.py

--- annabelle/modules/help.py
+++ annabelle/modules/help.py
@@ -3,7 +3,6 @@ from pyrogram.types import Message
 from config import HANDLER
 from annabelle.modules.helpmenu.help_menu import modules_help
 
-
 @Client.on_message(filters.command(["help", "h"], prefixes=f"{HANDLER}") & filters.me)
 async def help(client, message: Message):
     module_name = " ".join(message.command[1:])
@@ -40,11 +39,11 @@ async def help(client, message: Message):
         else:
             messages.append(tc)
         await message.edit(
-            messages[0], parse_mode="HTML", disable_web_page_preview=True
+            messages[0], parse_mode="html", disable_web_page_preview=True
         )
         messages.pop(0)
         for msg in messages:
-            await message.reply(msg, parse_mode="HTML", disable_web_page_preview=True)
+            await message.reply(msg, parse_mode="html", disable_web_page_preview=True)
     else:
         text = f"<b>Help for <i>{module_name}</i>\n\nUsage:</b>\n"
         found = False
@@ -56,11 +55,6 @@ async def help(client, message: Message):
                     cmd = list(u_cmd.items())[0]
                     text += f"""<code>{HANDLER + cmd[0]}</code> - <i>{cmd[1]}</i>\n"""
         if found:
-            await message.edit(text, parse_mode="HTML")
+            await message.edit(text, parse_mode="html")
         else:
             await message.edit(f"<b>Module <i>{module_name}</i> not found!</b>")
-
-
-modules_help.append(
-    {"help": [{"help [module name]": "To get help. Module name isn't required."}]}
-)
